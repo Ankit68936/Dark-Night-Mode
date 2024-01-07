45109064bf1db96d049e87d49169e03c71916ac0
Ankit68936 <113792754+Ankit68936@users.noreply.github.com>
2024-01-07 07:58:46 +0000

    Immediately woke up & Filter Function

---
+++ basics_1/test.js
@@ -0,0 +1,9 @@
+//immediately woke up function
+
+(function abc() {
+  //   console.log("hello World");
+})();
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const newNums = myNums.filter((num) => num > 4);
+console.log(newNums);
